0baedbe8751223b6583bb21df7a76802a0fe9d33
3nsar <84131267+3nsar@users.noreply.github.com>
2023-11-22 17:52:23 +0100

    commit

---
+++ typescript-threejs-page/src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+      <div className="hero-content">
+        <div className="hero-text">
+           <h1>WE HELP <span>NFT </span><>&#x1F9E0;</><br />PROJECTS LAUNCH</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
