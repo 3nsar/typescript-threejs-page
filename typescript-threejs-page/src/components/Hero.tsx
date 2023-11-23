dac6edd0ff75c62497bd7f1f1520282fb4c0c035
3nsar <84131267+3nsar@users.noreply.github.com>
2023-11-23 19:40:24 +0100

    commit

--- typescript-threejs-page/src/components/Hero.tsx
+++ typescript-threejs-page/src/components/Hero.tsx
@@ -2,18 +2,19 @@ import React from 'react'
 import { TbWorld } from "react-icons/tb";
 import { TbSunglasses } from "react-icons/tb";
 import { GiThorHammer } from "react-icons/gi";
+import trippy from "../assets/trippy-once.gif"
 
 const Hero = () => {
   return (
     <div className='hero-container'>
       <div className="hero-content">
         <div className="hero-text">
-           <h1>WE HELP <span>NFT </span><>&#x1F9E0;</><br />PROJECTS LAUNCH.</h1>
+           <h1>WE HELP <span>NFT </span><img src={trippy} alt="GIF" /><br />PROJECTS LAUNCH.</h1>
         </div>
         <div className="hero-info">
             <p><span className='hero-icon-color'><TbWorld /></span> <span className='hero-slash-color'>/</span> BASED IN AUSTRIA<br /> & WORKING WORLDWIDE</p>
             <p><span className='hero-icon-color'><TbSunglasses /></span> <span className='hero-slash-color'>/</span> YEARS OF BLOCK<br />CHAIN EXPERIENCE</p>
-            <p><span className='hero-icon-color'><GiThorHammer /></span> <span className='hero-slash-color'>/</span> ©2022 NFTDEVS<br /> ALL RIGHTS RESERVED</p>
+            <p><span className='hero-icon-color'><GiThorHammer /></span> <span className='hero-slash-color'>/</span> ©2023 NFTDEVS<br /> ALL RIGHTS RESERVED</p>
         </div>
       </div>
     </div>
