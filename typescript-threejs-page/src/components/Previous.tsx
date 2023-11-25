37ca5f2a776db2c05347f198184aa759089e9114
3nsar <84131267+3nsar@users.noreply.github.com>
2023-11-26 00:59:30 +0100

    commit

--- typescript-threejs-page/src/components/Previous.tsx
+++ typescript-threejs-page/src/components/Previous.tsx
@@ -1,30 +1,25 @@
 import React from 'react';
 import { MdDoubleArrow } from 'react-icons/md';
 import nft from '../images/white-nft.jpg';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Previous = () => {
   const clients = [
-    {
-      id: 1,
-      image: nft,
-      name: 'Croco',
-    },
-    {
-      id: 2,
-      image: nft,
-      name: 'TToco',
-    },
-    {
-      id: 3,
-      image: nft,
-      name: 'QRSTV',
-    },
-    {
-      id: 4,
-      image: nft,
-      name: 'JJOK',
-    },
+    { id: 1, image: nft, name: 'Croco' },
+    { id: 2, image: nft, name: 'TToco' },
+    { id: 3, image: nft, name: 'QRSTV' },
+    { id: 4, image: nft, name: 'JJOK' },
   ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
 
   return (
     <div className="previous-container">
@@ -37,15 +32,19 @@ const Previous = () => {
             PREVIOUS CLIENTS
           </h1>
         </div>
-        {clients.map((item) => (
-          <div
-            className='clients-container'
-            key={item.id}
-            style={{ backgroundImage: `url(${item.image})`}}
-          >
-            <p>{item.name}</p>
-          </div>
-        ))}
+
+        <div className="clients-container">
+        <Slider {...settings}>
+            {clients.map((item) => (
+              <div className="clients-content" key={item.id}>
+                <div className="clients-card">
+                  <img src={item.image} alt="pic" />
+                  <p>{item.name}</p>
+                </div>
+              </div>
+            ))}
+        </Slider>
+        </div>
       </div>
     </div>
   );
